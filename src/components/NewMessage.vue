de910ce20f64368eb0e58c234c8edbf65de81933
bankjirapan <jirapan_yankhan@hotmail.com>
2020-04-10 00:59:28 +0700

    {ADD} Send message to firebase

---
+++ src/components/NewMessage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="new-message">
+    <form @submit.prevent="addMessage">
+      <label for="new-message">New Message (enter to add) :</label>
+      <input type="text" name="new-message" v-model="newMessage" />
+      <p class="red-text" v-if="feedback">{{ feedback }}</p>
+    </form>
+  </div>
+</template>
+
+<script>
+import { messages, databaseURL } from "../firebase/config";
+export default {
+  name: "NewMessage",
+  props: ["name"],
+  data() {
+    return {
+      newMessage: null,
+      feedback: null
+    };
+  },
+  methods: {
+    addMessage() {
+      if (this.newMessage) {
+        messages
+          .push({
+            content: this.newMessage,
+            name: this.name,
+            timestamp: Date.now()
+          })
+          .catch(err => {
+            console.log(err);
+          });
+
+        // db.collection('messages').add({
+        //     content : this.newMessage,
+        //     name : this.name,
+        //     timestamp : Date.now()
+        // }).catch(err=>{
+        //     console.log(err);
+        // })
+      } else {
+        this.feedback = "You must enter a message in order to send one";
+      }
+      // console.log(this.newMessage,this.name,Date.now());
+    }
+  }
+};
+</script>
